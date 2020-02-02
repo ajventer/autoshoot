ba400c6607f91a786b58c9c9841a994ea9e0b592
A.J. Venter <ajventer@gmail.com>
2020-02-02 07:57:29 +0200

    Added MIT license

--- Autoshoot/auto_shoot.sh
+++ Autoshoot/auto_shoot.sh
@@ -3,12 +3,11 @@ COUNT=$1
 BULB=$2
 NAME=$3
 
-ps aux | fgrep "gphoto" | awk '{print $2}' | xargs kill -9 
-
 if test $# -ne 3 ; then
 	echo "Usage: $0 <number of shots> <exposure time> <filename base>"
 	exit
 fi
+ps aux | fgrep "gphoto" | awk '{print $2}' | xargs kill -9 
 
 C=0
 while test $C -ne $COUNT ; do
