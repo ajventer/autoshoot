e13e51bb192dca10d0e125e9295524c3077baa0d
AJ Venter <aj.venter@spandigital.com>
2020-02-02 22:13:31 +0200

    Better determination of camera readiness between shots

--- Autoshoot/auto_shoot.sh
+++ Autoshoot/auto_shoot.sh
@@ -11,7 +11,15 @@ ps aux | fgrep "gphoto" | awk '{print $2}' | xargs kill -9
 
 C=0
 while test $C -ne $COUNT ; do
-	gphoto2 -B $BULB --capture-image-and-download --no-keep --filename "$NAME-$C".cr2
-	sleep $((BULB+3))
+	echo
+	echo "Capturing shot $C"
+	echo
+	gphoto2 -B $BULB --capture-image-and-download --no-keep --filename "$NAME-$C".cr2 
+	echo "Waiting for camera to become available again"
+	while ! gphoto2 --list-config >/dev/null  ; do
+		echo -n "."
+		sleep 1
+	done
+	echo
 	C=$((C+1))
 done
