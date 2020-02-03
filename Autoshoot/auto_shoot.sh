38345b99e24edb7bc77cf01d608e1392ce8f14dd
AJ Venter <aj.venter@spandigital.com>
2020-02-03 15:18:37 +0200

    Use gphoto2's own continous shooting mode instead

--- Autoshoot/auto_shoot.sh
+++ Autoshoot/auto_shoot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 COUNT=$1
 BULB=$2
 NAME=$3
@@ -7,19 +7,7 @@ if test $# -ne 3 ; then
 	echo "Usage: $0 <number of shots> <exposure time> <filename base>"
 	exit
 fi
-ps aux | fgrep "gphoto" | awk '{print $2}' | xargs kill -9 
+ps aux | fgrep "gphoto" | fgrep -q 'fgrep' | awk '{print $2}' | xargs kill -9 
 
-C=0
-while test $C -ne $COUNT ; do
-	echo
-	echo "Capturing shot $C"
-	echo
-	gphoto2 -B $BULB --capture-image-and-download --no-keep --filename "$NAME-$C".cr2 
-	echo "Waiting for camera to become available again"
-	while ! gphoto2 --list-config >/dev/null  ; do
-		echo -n "."
-		sleep 1
-	done
-	echo
-	C=$((C+1))
-done
+gphoto2 -B $BULB --capture-image-and-download --no-keep  --frames $COUNT --interval 5 --filename $NAME-%n.%C
+
